f66916aa7ea04c89939280ef8b01330f66e2b7f4
miguelsuau <miguel.suau@gmail.com>
2021-08-24 12:30:03 +0200

    256 HIDDEN MEMORY SIZE

--- PPO/policy.py
+++ PPO/policy.py
@@ -6,7 +6,7 @@ from torch.distributions import Categorical
 import numpy as np
 
 HIDDEN_SIZE = 512
-HIDDEN_MEMORY_SIZE = 512
+HIDDEN_MEMORY_SIZE = 256
 NUM_FILTERS = [64, 64, 32]
 KERNEL_SIZE = [8, 4, 2]
 

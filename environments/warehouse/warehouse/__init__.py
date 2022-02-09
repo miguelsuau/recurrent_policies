061d354d059a016bce1a3a46ebf31bed90fc07c4
Miguel Suau <miguel.suau@gmail.com>
2022-02-09 17:38:54 +0100

    removed dependencies

--- environments/warehouse/warehouse/__init__.py
+++ environments/warehouse/warehouse/__init__.py
@@ -1,17 +1,5 @@
 from gym.envs.registration import register
 register(
-    id='global-warehouse-v0',
-    entry_point='warehouse.envs:GlobalWarehouse',
-)
-register(
-    id='local-warehouse-v0',
-    entry_point='warehouse.envs:LocalWarehouse',
-)
-register(
-    id='global-mini-warehouse-v0',
-    entry_point='warehouse.envs:MiniWarehouse',
-)
-register(
     id='mini-warehouse-v0',
     entry_point='warehouse.envs:MiniWarehouse',
 )

d2714e6329c64a3620b708c6e560ee2c87562c70
Miguel Suau <miguel.suau@gmail.com>
2022-02-17 09:57:56 +0100

    mini warehouse

--- environments/warehouse/warehouse/envs/mini_warehouse.py
+++ environments/warehouse/warehouse/envs/mini_warehouse.py
@@ -277,8 +277,8 @@ class MiniWarehouse(gym.Env):
                 #     reward = 0.1
                 #     self.items.remove(item)
                 #     break
-                reward = item_waiting_times[index]/max(item_waiting_times)
-                # reward = 1
+                # reward = item_waiting_times[index]/max(item_waiting_times)
+                reward = 1.0
                 # self.items.remove(item)
                 # reward += 10
                 # reward += 1/item.get_waiting_time

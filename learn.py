d980dae4eb308fd59af2d919c357f5449405ae66
Miguel Suau <miguel.suau@gmail.com>
2022-03-15 17:30:02 +0100

    changed features

--- learn.py
+++ learn.py
@@ -91,10 +91,10 @@ class FeatureVectorWrapper(gym.core.ObservationWrapper):
 
     def observation(self, obs):
         obs = obs[:,:,0].astype(int)
-        # obs[np.where(obs==1)] = 0
-        # obs[np.where(obs==2)] = 1
-        # obs[np.where(obs==5)] = 2
-        # obs[np.where(obs==6)] = -2
+        obs[np.where(obs==1)] = 0
+        obs[np.where(obs==2)] = 1
+        obs[np.where(obs==5)] = 2
+        obs[np.where(obs==6)] = -2
         return np.reshape(obs,-1)
 
 class Experiment(object):

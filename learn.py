a470a0f2f6b497792f316399ade4f66d751b5cb0
Miguel Suau <miguel.suau@gmail.com>
2022-03-24 14:06:19 +0100

    S17 random

--- learn.py
+++ learn.py
@@ -260,8 +260,8 @@ class Experiment(object):
                 #     tags={'wrapper_config.TimeLimit.max_episode_steps': 5000}
                 # )
                 # env = gym.make(id='MiniGrid-RedBlueDoors-6x6-v0')
-                # env = gym.make(id='MiniGrid-MemoryS13Random-v0')
-                env = gym.make(id='MiniGrid-MemoryS13-v0')
+                env = gym.make(id='MiniGrid-MemoryS17Random-v0')
+                # env = gym.make(id='MiniGrid-MemoryS13-v0')
                 # env = RGBImgPartialObsWrapper(env)
                 # env = wrappers.TimeLimit(env, max_episode_steps=1280)
                 env = ImgObsWrapper(env) # Get rid of the 'mission' field
